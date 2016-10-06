3f6b6bd7ff2834b7a95187a0e2b571f43dd1ef63
Will Zhang <will.zhang80@gmail.com>
2016-10-06 17:14:43 -0600

    Create RecommendationPredictor object

--- recsys.py
+++ recsys.py
@@ -1,80 +1,109 @@
-import csv
 import numpy as np
 import pandas as pd
 
 # Load data files
-trainDataFile = './data/toy_train.csv'
-testDataFile = './data/toy_test.csv'
+from numpy.core.tests.test_numerictypes import read_values_nested
+
+train_data_file = './data/toy_train.csv'
+test_data_file = './data/toy_test.csv'
 # trainDataFile = './data/restaurant_train.csv'
 # testDataFile = './data/restaurant_test.csv'
 
-trainData = pd.read_csv(trainDataFile)
-trainData.columns = ['user_id', 'item_id', 'rating']
+train_data = pd.read_csv(train_data_file)
+train_data.columns = ['user_id', 'item_id', 'rating']
 
-testData = pd.read_csv(testDataFile)
-testData.columns = ['user_id', 'item_id', 'rating']
+test_data = pd.read_csv(test_data_file)
+test_data.columns = ['user_id', 'item_id', 'rating']
 
-# Compare users and determine if any test users are not in the training data set
-uniqueTrainUser = trainData.user_id.unique()
-uniqueTestUser = testData.user_id.unique()
-uniqueCompareMask = np.in1d(uniqueTestUser, uniqueTrainUser, invert=True)
-
-# Find test users that are not in the training data
-testNotInTrain = uniqueTestUser[uniqueCompareMask]
-assert len(testNotInTrain) == 0
+# Compare training data users and test data users and determine if any test users are not in the training data set
+unique_train_user = train_data.user_id.unique()
+unique_test_user = test_data.user_id.unique()
+unique_compare_mask = np.in1d(unique_test_user, unique_train_user, invert=True)
+assert len(unique_test_user[unique_compare_mask]) == 0
 
 '''
 From the output we know that all test data set users exists in the training data.
-To optimize the computation, we will first find the 40 nearest neighbours for each user using the trainning data set
+To optimize the computation, we will first find the 40 nearest neighbours for each user using the training data set
 and we calculate the prediction with test data
 '''
 
-# Create rating matrix with training data
-trainMatrix = trainData.pivot_table(values = 'rating', index = 'user_id', columns = 'item_id')
-assert trainMatrix.shape == (len(trainData.user_id.unique()), len(trainData.item_id.unique()))
-
-print trainMatrix.shape
-
-# Calculate average rating for all users
-trainUserMean = pd.DataFrame(data=trainMatrix.mean(axis=1), index=trainMatrix.index)
-
-# Define cosine similarity function
-def cosineSim(u1Rating, u2Rating):
-    u1RatingSqrtSum = np.sum(u1Rating.apply(lambda x: x**2))
-    u2RatingSqrtSum = np.sum(u2Rating.apply(lambda x: x ** 2))
-    return np.sum(u1Rating * u2Rating) / np.sqrt(u1RatingSqrtSum * u2RatingSqrtSum)
-
-# Find 40 nearest neighbours with cosine sim 2.5 (train)
-
-def calculateUserSimMatrix(mtrx):
-    simMatrix = pd.DataFrame()
-    for i in range(0, len(mtrx)):
-        u = mtrx[mtrx.index == mtrx.index[i]].squeeze()
-        simSerie = mtrx.apply(lambda x: cosineSim(u, x.squeeze()), axis =1)
-        simMatrix[u.name] = simSerie
-    return simMatrix
-
-simMatrix = calculateUserSimMatrix(trainMatrix)
-
-# TODO get user1 top 40
-simMatrix.ix['user1'].order(ascending=False)[1:41]
+# Create rating matrix from training data
+training_data_matrix = train_data.pivot_table(values='rating', index='user_id', columns='item_id')
+assert training_data_matrix.shape == (len(train_data.user_id.unique()), len(train_data.item_id.unique()))
+print 'Training data matrix shape: %s X %s' % (training_data_matrix.shape[0], training_data_matrix.shape[1])
 
 
+# Create a recommendation predictor object to predict user ratings
+class RecommendationPredictor(object):
+    # Define cosine similarity function
+    @staticmethod
+    def __cosine_sim(u1_rating, u2_rating):
+        u1_rating_sqrt_sum = np.sum(u1_rating.apply(lambda x: x ** 2))
+        u2_rating_sqrt_sum = np.sum(u2_rating.apply(lambda x: x ** 2))
+        return np.sum(u1_rating * u2_rating) / np.sqrt(u1_rating_sqrt_sum * u2_rating_sqrt_sum)
+
+    def __init__(self, train_matrix):
+        self.train_matrix = train_matrix
+
+        # Calculate average rating for all users
+        self.user_mean_ratings = pd.DataFrame(data=train_matrix.mean(axis=1), index=train_matrix.index)
+
+        # Calculate similarities between all users
+        self.similarity_matrix = self.__calculate_user_sim_matrix(train_matrix)
+
+    # Define function to calculate similarities between all users using formula 2.5 from the text book
+    def __calculate_user_sim_matrix(self, mtrx):
+        mtrx_df = pd.DataFrame()
+        for i in range(0, len(mtrx)):
+            u = mtrx[mtrx.index == mtrx.index[i]].squeeze()
+            sim_serie = mtrx.apply(lambda x: self.__cosine_sim(u, x.squeeze()), axis=1)
+            mtrx_df[u.name] = sim_serie
+        return mtrx_df
+
+    # Define prediction function using formula 2.3 from the text book
+    def predict_rating(self, user_name, item_name):
+        try:
+            # Get the user's average ratings
+            u_avg_rate = self.user_mean_ratings.ix[user_name]
+
+            # Get the user's 40 nearest neighbors from the similarity_matrix
+            # Excluding oneself
+            neighbors = similarity_matrix.ix[user_name].order(ascending=False)[1:41]
+            neighbors = neighbors.to_frame(name='sim_score')
+
+            # Get average ratings of all the neighbors
+            neighbors['avg_rating'] = self.user_mean_ratings.ix[neighbors.index]
+
+            # Get ratings of all the neighbors on the specific item for prediction
+            neighbors['product_rating'] = self.train_matrix.ix[neighbors.index, item_name]
+
+            # Calculate prediction with formula 2.3
+            n_nominator = (neighbors['sim_score'] * (neighbors['product_rating'] - neighbors['avg_rating'])).sum(skipna=True)
+
+            pred_rating = u_avg_rate + n_nominator / (neighbors['sim_score']).sum(skipna=True)
+
+            return pred_rating
+
+        except KeyError as e:
+            print e.message
+            raise
+
+    def get_user_mean_ratings(self):
+        return self.user_mean_ratings
+
+    def get_similarity_matrix(self):
+        return self.similarity_matrix
+
+
+rec_predictor = RecommendationPredictor(training_data_matrix)
+
+# TODO
+# Predict user ratings on test data set
+print test_data.ix[1:10]
+rec_predictor.predict_rating('user39', 'item7')
+
+
+# Compare result using RMSE
 
 
 
-
-
-
-
-# http://stackoverflow.com/questions/25736861/python-pandas-finding-cosine-similarity-of-two-columns
-trainMatrix.ix[1:10,1:10]
-trainMatrix.ix['user39', 'item17']
-trainMatrix.ix['user12', 'item11']
-
-trainMatrix.ix['user39'] * trainMatrix.ix['user39'] # trainMatrix.ix['user39'] ** 2
-
-# Prediction based on 40 nearest neighbours 2.3 (test)
-def predictRating(uer_id, item_id):
-    print uer_id + " " + item_id
-    return 3
