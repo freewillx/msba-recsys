14621ac168f476c0c959cc3b4c77e1272be0f36e
Will Zhang <will.zhang80@gmail.com>
2016-12-05 17:18:35 -0700

    Ensemble CF & MF with plot

--- recsys.py
+++ recsys.py
@@ -123,36 +123,41 @@ def predict_new_mf_ratings(new_data, train_matrix, n_features, reg=0):
     pred_df['predict_rating'] = None
 
     for row in new_data.itertuples():
-
         logger.debug('Processing entry %d' % (row[0]))
         uid = row[1]
         bid = row[2]
 
-        try:
-            # Find users who have also rated this business
-            rate_matrix = train_matrix.loc[train_matrix[bid].notnull()]
+        # Use 40 neighbours for rating predictions
+        neighbor_scores = rated_neighbors_sim_score(train_matrix, uid, bid, n=40)
+        if (neighbor_scores is not None):
+            try:
+                # Find users who have also rated this business
+                rate_matrix = train_matrix.loc[neighbor_scores.index]
 
-            # If the predicting user is not in the list, add to decomposing matrix
-            if uid not in rate_matrix.index:
-                rate_matrix = rate_matrix.append(train_rating_matrix.loc[uid])
+                # If the predicting user is not in the list, add to decomposing matrix
+                if uid not in rate_matrix.index:
+                    rate_matrix = rate_matrix.append(train_matrix.loc[uid])
 
-            # Number of users in the decomposing matrix should be smaller than the number of features
-            if (n_features > rate_matrix.shape[0]):
-                n_features = rate_matrix.shape[0]
+                # Number of users in the decomposing matrix should be smaller than the number of features
+                if (n_features > rate_matrix.shape[0]):
+                    n_features = rate_matrix.shape[0]
 
-            # Matrix factorization with given NMF model
-            model = NMF(init='random', n_components=n_features, alpha=reg, random_state=3, max_iter=500)
-            user_feature_mtrx = model.fit_transform(rate_matrix.to_sparse(fill_value=0))
-            item_feature_mtrx = model.components_
+                rate_matrix = rate_matrix.dropna(axis=1, how='all').fillna(0)
+                # Matrix factorization with given NMF model
+                model = NMF(init='nndsvd', n_components=n_features, alpha=reg, random_state=3, max_iter=500)
+                user_feature_mtrx = model.fit_transform(rate_matrix)
+                item_feature_mtrx = model.components_
 
-            # Compute prediction matrix
-            pred_matrix = np.dot(user_feature_mtrx, item_feature_mtrx)
-            pred_matrix = pd.DataFrame(pred_matrix, index=rate_matrix.index, columns=rate_matrix.columns)
+                # Compute prediction matrix
+                pred_matrix = np.dot(user_feature_mtrx, item_feature_mtrx)
+                pred_matrix = pd.DataFrame(pred_matrix, index=rate_matrix.index, columns=rate_matrix.columns)
 
-            pred = pred_matrix.loc[uid][bid]
+                pred = pred_matrix.loc[uid][bid]
 
-        except KeyError:
-            pred = None
+            except KeyError:
+                pred = np.nan
+        else:
+            pred = np.nan
 
         pred_df.set_value(row[0], 'predict_rating', pred)
 
@@ -215,74 +220,72 @@ logger.info('Calculated RMSE using CF method: %f ' % (cf_rmse))
 
 
 """Calculate prediction with MF method"""
-# Select the number of latent features = 50
-# Regularization coefficient 0.02
-
-
-'''
-Grid Search - regularization coefficient 0.0001 ~ 0.05, number of features 2~20
-Determined the best number of features 0.0002, 2 features with RMSE of 2.219413
-
-
+#Grid Search - regularization coefficient 0.001 ~ 0.05, number of features 2~20
+#Determined the best number of features 0.001, 10 features with RMSE of 1.747664
 rmse_dict = {}
-for reg in range(0,7):
-    reg_coefficent = float(reg)*0.0001
+for reg in range(0,5):
+    reg_coefficent = float(reg)*0.001
     logger.info('Computing RMSE with Alpha %f' % (reg_coefficent))
     feature_dict = {}
-    for n in range(2,5):
-        mf_predictions = predict_new_mf_ratings(test_data, train_rating_matrix, n_features = n, reg=reg_coefficent)
+    for n in range(2,21):
+        mf_predictions = predict_new_mf_ratings(test_data.head(10), test_data.pivot_table(values='rating', index='user_id', columns='business_id'), n_features = n, reg=reg_coefficent)
         mf_rmse = calculate_rmse(mf_predictions['rating'], mf_predictions['predict_rating'])
         feature_dict[n] = mf_rmse
     rmse_dict[reg_coefficent] = feature_dict
 
-min_alpha = 100;
-min_n = 100
-min_rmse = 100
+min_alpha = None;
+min_n = None
+min_rmse = None
 for reg in rmse_dict:
     feature_dict = rmse_dict[reg]
     for f in feature_dict:
-        if min_rmse > feature_dict[f]:
+        if min_rmse == None or min_rmse > feature_dict[f]:
             min_alpha = reg
             min_n = f
             min_rmse = feature_dict[f]
         logger.info('Alpha %f RMSE for %d features is %f' % (reg, f, feature_dict[f]))
 
-'''
+logger.info('Best alpha: %f, best number of features: %f with RMSE of%f' % (min_alpha, min_n, min_rmse))
 
-mf_predictions = predict_new_mf_ratings(test_data, train_rating_matrix, n_features = 2, reg=0.0002)
+# Select the number of latent features = 10
+# Regularization coefficient 0.001
+mf_predictions = predict_new_mf_ratings(test_data, test_data.pivot_table(values='rating', index='user_id', columns='business_id'), n_features = 10, reg=0.001)
 mf_rmse = calculate_rmse(mf_predictions['rating'], mf_predictions['predict_rating'])
 logger.info('Calculated RMSE using MF method: %f ' % (mf_rmse))
 
-'''
-Apply ensemble method to combine CF ratings and MF ratings with blending ratings
-'''
+'''Apply ensemble method to combine CF ratings and MF ratings with blending ratings'''
 # Ensemble with weighted average
 cf_predictions=cf_predictions.rename(columns = {'predict_rating':'cf_predict_rating'})
 mf_predictions=mf_predictions.rename(columns = {'predict_rating':'mf_predict_rating'})
 
 logger.info('Number of predicted rating with CF method: %f ' % (len(cf_predictions)))
 logger.info('Number of predicted rating with MF method: %f ' % (len(mf_predictions)))
-cf_predictions.head(10)
-mf_predictions.head(10)
 
 ensemble_result = pd.merge(cf_predictions, mf_predictions, how='outer', on=['user_id', 'business_id', 'rating'])
-ensemble_result.head(10)
 
 
 # Grid search to find the best alpha to combine CF and MF with
 ensemble_predict_df = pd.DataFrame(ensemble_result[['user_id', 'business_id', 'rating']])
 
+plot_alpha_serie = []
+plot_rmse_serie = []
 for i in range(0,11):
+
     alpha = i * 0.1
-    logger.info('Test alpha %f ' % (alpha))
-
     ensemble_predict_df['ensemble_rating'] = (ensemble_result['cf_predict_rating'] * alpha) + (ensemble_result['mf_predict_rating'] * (1-alpha))
-
     ensemble_rmse = calculate_rmse(ensemble_predict_df['rating'], ensemble_predict_df['ensemble_rating'])
-    logger.info('Calculated RMSE using Ensemble method: %f ' % (ensemble_rmse))
 
-'''
-Best result
-2016-12-04 02:26:15,005 - INFO - Test alpha 1.000000
-2016-12-04 02:26:15,008 - INFO - Calculated RMSE using Ensemble method: 0.909691
-'''
+    plot_alpha_serie.append(alpha)
+    plot_rmse_serie.append(ensemble_rmse)
+    logger.info('Ensemble RMSE with Alpha %f is: %f ' % (alpha, ensemble_rmse))
+
+'''Plot Ensemble RMSE graph - Best result of Ensemble RMSE with Alpha 0.800000 is: 0.868461'''
+
+import matplotlib.pyplot as plt
+
+dots = plt.plot(plot_alpha_serie, plot_rmse_serie, 'ro')
+line = plt.plot(plot_alpha_serie, plot_rmse_serie, color = 'r', linewidth=2.0)
+plt.xlabel('Alpha')
+plt.ylabel('Ensemble RMSE value')
+plt.show()
+
